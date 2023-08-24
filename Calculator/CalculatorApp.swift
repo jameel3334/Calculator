d14b6b697104dffe8abd2fb96702e39b15dcba2d
Mohammed Jameeluddin <jameel3334@gmail.com>
2023-08-24 15:51:08 -0400

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Mohammed Jameeluddin on 8/24/23.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
